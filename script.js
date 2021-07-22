604fd3d0e293f972fe0fb237d166d9bdd1d825f6
Rodolfo Vieira <rodolfovieira95@outlook.com>
2021-07-21 22:07:50 -0300

    add dw14

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function handleClick() {
-  document.getElementById("item2").style.backgroundColor = "red";
+function handleChange(param) {
+  if (param == "metro") {
+    const metro = document.getElementById("metro").value;
+    document.getElementById("kilometro").value = 1000 * metro;
+  } else {
+    const kilometro = document.getElementById("kilometro").value;
+    document.getElementById("metro").value = kilometro / 1000;
+  }
 }
